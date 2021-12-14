ad43be9299f608020234ab28d5a1be496a9b0503
maksimvelma <maks.velma@yandex.ru>
2021-12-14 15:55:36 +0200

    Create task1.cs

---
+++ task1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace lab3
+{
+    class Person
+    {
+        public string name { get; set; }
+        int age;
+        public int setAge
+        {
+            set { age = value; }
+        }
+        public int Greet
+        {
+            get { return age; }
+        }
+    }
+    class Student : Person
+    {
+        public void Study()
+        {
+            Console.WriteLine("I'm studying");
+        }
+        public void ShowAge(int x)
+        {
+            Console.WriteLine($"My age is: " + x + " years old");
+        }
+    }
+    class Professor : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("I'm explaining");
+        }
+    }
+    class StudentProfessorTest
+    {
+        string name { get; }
+        public StudentProfessorTest(string _name)
+        {
+            name = _name;
+        }
+        public void Main()
+        {
+            Console.WriteLine("Добрий день сказав " + name);
+
+            var student = new Student();
+            student.setAge = 18;
+            student.name = "Максим";
+            Console.WriteLine("Привiт мене звати " + student.name + ", я студент");
+            student.ShowAge(student.Greet);
+
+            var teacher = new Professor();
+            teacher.setAge = 40;
+            teacher.name = "Вiталiй";
+            Console.WriteLine("Добрий день, я " + teacher.name + ", менi " + teacher.Greet + " рокiв");
+            teacher.Explain();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new StudentProfessorTest("Максим");
+            test.Main();
+        }
+    }
+}
